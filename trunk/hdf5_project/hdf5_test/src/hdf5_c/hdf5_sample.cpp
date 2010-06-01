99f67f6362ae319b8c4f9886768858f63c6759c1
kevinkao.gph <kevinkao.gph@0a44f7b2-fd79-11de-a344-e7ee0817407e>
2010-06-01 22:29:18 +0000

    function

--- trunk/hdf5_project/hdf5_test/src/hdf5_c/hdf5_sample.cpp
+++ trunk/hdf5_project/hdf5_test/src/hdf5_c/hdf5_sample.cpp
@@ -1,14 +1,32 @@
-   #include "hdf5.h"
-#define FILE "file.h5"
+#include "hdf5.h"
+#include "hdf5_hl.h"
 
-   int main() {
+#define RANK 2
+int testhdf();
 
-      hid_t       file_id;   /* file identifier */
-      herr_t      status;
+int main( void )
+{
+int errorCode;
+	
+errorCode = testhdf();
+}
 
-      /* Create a new file using default properties. */
-      file_id = H5Fcreate(FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-      /* Terminate access to the file. */
-      status = H5Fclose(file_id); 
-   }
+int testhdf()
+{
+ hid_t       file_id;
+ hsize_t     dims[RANK]={2,3};
+ int         data[6]={1,2,3,4,5,6};
+ herr_t      status;
+
+ /* create a HDF5 file */
+ file_id = H5Fcreate ("ex_lite1.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+ /* create and write an integer type dataset named "dset" */
+ status = H5LTmake_dataset(file_id,"/dset",RANK,dims,H5T_NATIVE_INT,data);
+
+ /* close file */
+ status = H5Fclose (file_id);
+
+ return 0;
+}
