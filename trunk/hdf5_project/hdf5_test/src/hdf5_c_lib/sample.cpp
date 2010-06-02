4c066e8510a77f015a9ece0b863f746e71cfb1ea
kevinkao.gph <kevinkao.gph@0a44f7b2-fd79-11de-a344-e7ee0817407e>
2010-06-02 22:49:00 +0000

    pass float array.

--- trunk/hdf5_project/hdf5_test/src/hdf5_c_lib/sample.cpp
+++ trunk/hdf5_project/hdf5_test/src/hdf5_c_lib/sample.cpp
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include "hdf5.h"
 #include "hdf5_hl.h"
-#define RANK 2
+#define RANK2 2
+#define RANK1 1
 extern "C" void c_routine (
-                        int int_arg,
+                        int int_arg_by_val,
+						int int_array[2],
                         char* input_text,
                         char* output_text
                         )
 
 {
-    sprintf(output_text,"%s%i ",input_text,int_arg);
+	int n1 = int_array[0];
+	int n2 =  int_array[1];
+    sprintf(output_text,"%s%i%s%i",input_text,n1,"_", n2);
 }
 
-extern "C" void testhdf()
+extern "C" void testhdf(float* data_float, int size_data_float)
 {
  hid_t       file_id;
- hsize_t     dims[RANK]={2,3};
+ hsize_t     dims[RANK2]={2,3};
  int         data[6]={1,2,3,4,5,6};
  herr_t      status;
+ hsize_t     dims4[RANK1]={size_data_float};
 
  /* create a HDF5 file */
- file_id = H5Fcreate ("ex_lite1.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+ file_id = H5Fcreate ("calledFromFortran.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
  /* create and write an integer type dataset named "dset" */
- status = H5LTmake_dataset(file_id,"/dset",RANK,dims,H5T_NATIVE_INT,data);
+ status = H5LTmake_dataset(file_id,"/dset",RANK2,dims,H5T_NATIVE_INT,data);
+ status = H5LTmake_dataset(file_id,"/another",RANK1,dims4,H5T_NATIVE_FLOAT,data_float);
 
  /* close file */
  status = H5Fclose (file_id);
