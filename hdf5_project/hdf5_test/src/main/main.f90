116dea232b4e26cbf342c3e456d55df5b5712f2d
kevinkao.gph <kevinkao.gph@0a44f7b2-fd79-11de-a344-e7ee0817407e>
2010-06-02 23:41:22 +0000

    organize c interface into a module.

--- hdf5_project/hdf5_test/src/main/main.f90
+++ hdf5_project/hdf5_test/src/main/main.f90
@@ -1,33 +1,9 @@
 PROGRAM fortran_call_c
-IMPLICIT NONE
 
-! Declare the interface for the C routine we'll call
-!
-INTERFACE
-    ! The BIND(C) tells the compiler that this is an "interoperable"
-    ! procedure.  The compiler adjusts the naming conventions as
-    ! appropriate for the companion C processor.
-    SUBROUTINE c_routine (int_arg, int_array, str_in, str_out) BIND(C)
-       USE,INTRINSIC :: ISO_C_BINDING  ! Declares C kinds
+use hdf5_C_interface
 
-       ! First argument is a C "int", passed by value
-       INTEGER(C_INT), VALUE,INTENT(IN) :: int_arg
-       INTEGER(C_INT), INTENT(IN) :: int_array(1:2)
-       ! Second and third arguments are C "char *", represented
-       ! in Fortran by an array of single characters of kind C_CHAR.
-       ! Note that the language allows passing a regular CHARACTER
-       ! variable to such an argument.
-       CHARACTER(KIND=C_CHAR),DIMENSION(*) :: str_in,str_out
-    END SUBROUTINE c_routine
 
-    SUBROUTINE testhdf( float_array, size_float_array ) BIND(C)
-       USE,INTRINSIC :: ISO_C_BINDING  ! Declares C kinds
-       REAL(C_FLOAT), DIMENSION(*), INTENT(IN) :: float_array
-       INTEGER(C_INT), VALUE,INTENT(IN) :: size_float_array
-    END SUBROUTINE testhdf
-
-END INTERFACE
-
+implicit none
 CHARACTER(80) OUTPUT_TEXT
 INTEGER IN_ARG, int_array(2), OUTPUT_LEN,i
 CHARACTER(80) INPUT_TEXT
