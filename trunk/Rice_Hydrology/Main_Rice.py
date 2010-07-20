43b93e8889329fb913d79f92ec4c99fb28276c13
kevinkao.gph <kevinkao.gph@0a44f7b2-fd79-11de-a344-e7ee0817407e>
2010-07-20 00:21:27 +0000

    todo: fix RR NonPond

---
+++ trunk/Rice_Hydrology/Main_Rice.py
@@ -0,0 +1,36 @@
+import os #os.getcwd()
+import tableRain
+import ETr, LP, DP, AWr, RR
+#import ETr            # output
+#from numpy import *   # bug in ironclad
+#from functions import *
+from constants import *
+
+ETr_File = open('ETr_out.out','w') 
+AWr_File = open('AWr_out.out','w') 
+LP_File = open('LP_out.out','w') 
+DP_File = open('DP_out.out','w') 
+
+calendar_year=1934
+iyr=calendar_year-tableRain.START_YEAR+1
+
+LP.find()
+DP.find()
+
+ETr.find()
+#print ETr.Burn
+AWr.find()
+
+#print AWr.Pond[calendar_year-tableRain.START_YEAR+1]
+#print AWr.NonPond
+
+#print LP.Grow
+
+RR.find()
+print RR.NonPond[iyr]
+
+
+LP.record(LP_File)
+DP.record(DP_File)
+ETr.record(ETr_File)
+AWr.record(AWr_File)
---
+++ trunk/Rice_Hydrology/RR.py
@@ -0,0 +1,47 @@
+# Rainfall-Runoff
+
+import os #os.getcwd()
+import area, lookup, tableRain, tableAW    # user input
+import ETr, AWr, LP, DP
+#import DP              # output
+#from numpy import *   # bug in ironclad
+from functions import *
+from constants import *
+
+#outFile = open('DP_out', 'w')
+
+Pond = zeros(12)
+NonPond = zeros_2D(85,12)
+Grow = zeros_2D(85,12)
+Burn = zeros_2D(85,12)
+Sum = zeros_2D(85,12)
+
+def find():
+
+    for DU_id in (75,75):
+        for mon in range(1, 12+1): # 1 to 12
+            #Pond[mon] = lookup.Pond[mon] * DP_RATE * area.Pond[DU_id]
+            #Grow[mon] = lookup.Grow[mon] * tableRain.Rain[mon] * area.Total[DU_id]
+            #NonPond[mon]  = 0
+            k=3
+    
+        for calendar_year in range(1922, 2006):
+            iyr = calendar_year - tableRain.START_YEAR + 1
+            for mon in range(1, 12+1): # 1 to 12
+                Grow[iyr][mon] = lookup.Grow[mon] * tableRain.Rain[iyr][mon] * area.Total[DU_id]
+                
+                # NonPond
+                OW = tableAW.NonPond[mon]*area.NonPond[DU_id]
+                ET_met = min( OW+AWr.NonPond[mon], ETr.NonPond[mon])
+                NonPond[iyr][mon] = OW + lookup.NonGrow[mon]*tableRain.Rain[iyr][mon]*area.NonPond[DU_id]-ET_met-LP.NonPond[mon] - DP.NonPond[mon]
+                NonPond[iyr][mon] = max(0, NonPond[iyr][mon])
+                
+
+def record(outFile):
+    
+    for calendar_year in range(1922, 2006):
+        iyr = calendar_year-tableRain.START_YEAR+1
+        for mon in range(1,13):
+            outFile.writelines( str(calendar_year)+'  '+str(mon) +'  '+ str(Sum[iyr][mon])+'\n' )
+
+
